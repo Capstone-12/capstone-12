6ff8d43a2832d5b6f7851debc2a7c3351f51f93f
Faboya Basil korede <106436258+Faboya-korede@users.noreply.github.com>
2023-06-28 13:45:59 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 
 terraform {
   backend "s3" {
-    bucket = "teslim-s3terra"
+    bucket = "capstonegithub12"
     key    = "terraform.tfstate"
-    region = "eu-west-2"
+    region = "us-east-1"
   }
 }
